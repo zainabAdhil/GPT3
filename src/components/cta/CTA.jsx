cf91c728110d3a418f5809f7dd1aba8d71e50537
zainabAdhil <zaiaadh240@gmail.com>
2024-05-17 11:02:21 +0530

    Commit Changes

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './cta.css';
 
-const CTA = () => {
-  return (
-    <div>
-      CTA
+const CTA = () => (
+  <div className="gpt3__cta">
+    
+    <div className="gpt3__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
     </div>
-  )
-}
 
-export default CTA
+    <div className="gpt3__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+);
+
+export default CTA;
